41dd3efbfe01f18a5a6b5bac7ae1fdf7021872b0
Richard <richl.laconte@gmail.com>
2019-10-03 14:26:18 -0400

    make it better

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ app.get('/link/:id/', function(req, res) {
             if (collection.find({ id })) {
                 collection.find({ id }).toArray(function(err, docs) {
                 
-                    if (docs === !null) {
+                    
                         if (docs[0] === !null) {
                             if (docs[0].redirect === !null) {
                                 res.redirect("https://" + docs[0].redirect);
@@ -114,9 +114,7 @@ app.get('/link/:id/', function(req, res) {
                         } else {
                             res.send("oops3");
                         }
-                    } else {
-                        res.send("oops2");
-                    }
+                    
                     
                 })
             } else {
