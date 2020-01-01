473639b83f47c0c16e65e2e3e1a9bba016e06ecc
Richard LaConte <38797549+pakjones@users.noreply.github.com>
2020-01-01 14:36:21 -0500

    Made test script for SS

---
+++ SSFormScript.js
@@ -0,0 +1,7 @@
+let endpoint;
+
+let script = document.getElementById("ezlinkssformscript");
+let parent = script.parentNode;
+
+let form = document.createElement("DIV");
+form.innerHTML = '<form><label>Name</label><input type="text"></input><button type="submit" value="submit">Submit</button></form>';
